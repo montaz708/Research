f453a29a214105258db6d9ac10dc3ef9ace38076
Alex Vazquez <alex.vazquez708@gmail.com>
2019-03-07 16:42:18 -0500

    First commit, first profile and some bash scripts

--- profile.py
+++ profile.py
@@ -5,38 +5,9 @@ import geni.rspec.igext as IG
 pc = portal.Context()
 request = rspec.Request()
 
-"""
-pc.defineParameter("workerCount",
-                   "Number of Hadoop DataNodes",
-                   portal.ParameterType.INTEGER, 3)
-"""
-"""
-pc.defineParameter("controllerHost", "Name of NameNode",
-                   portal.ParameterType.STRING, "node0", advanced=True,
-                   longDescription="The short name of the Hadoop NameNode.  You shold leave \
-                   this alone unless you really want the hostname to change.")
-"""
 
-# params = pc.bindParameters()
+params = pc.bindParameters()
 
-tourDescription = "This profile provides a configurable Hadoop testbed with one NameNode \
-and customizable number of DataNodes."
-
-tourInstructions = \
-  """
-### Basic Instructions
-Once your experiment nodes have booted, and this profile's configuration scripts \
-have finished deploying Hadoop inside your experiment, you'll be able to visit 
-[the HDFS Web UI](http://{host-%s}:9870) (approx. 5-15 minutes).  
-""" % (params.controllerHost)
-
-#
-# Setup the Tour info with the above description and instructions.
-#  
-tour = IG.Tour()
-tour.Description(IG.Tour.TEXT,tourDescription)
-tour.Instructions(IG.Tour.MARKDOWN,tourInstructions)
-request.addTour(tour)
 
 # Create a link with type LAN
 link = request.LAN("lan")
@@ -49,34 +20,30 @@ for i in range(2):
     iface.component_id = "eth1"
     iface.addAddress(rspec.IPv4Address("192.168.1." + str(i + 1), "255.255.255.0"))
     link.addInterface(iface)
-    
+
     node.addService(rspec.Execute(shell="/bin/sh",
                                   command="sudo wget https://www-eu.apache.org/dist/spark/spark-2.4.0/spark-2.4.0-bin-hadoop2.7.tgz"))
     node.addService(rspec.Execute(shell="/bin/sh",
                                   command="sudo tar -xzf spark-2.4.0-bin-hadoop2.7.tgz -C /opt/"))
     node.addService(rspec.Execute(shell="/bin/sh",
-                                  command="sudo cp /local/repository/master /opt/hadoop-3.0.0/ect/hadoop/"))
+                                  command="sudo cp /local/repository/spark-env.sh /opt/spark-2.4.0-bin-hadoop2.7/conf/spark-env.sh"))
     node.addService(rspec.Execute(shell="/bin/sh",
-                                  command="sudo cp /local/repository/slaves /opt/hadoop-3.0.0/etc/hadoop/workers"))
-    node.addService(rspec.Execute(shell="/bin/sh",
-                                  command="sudo cp /local/repository/core-site.xml /opt/hadoop-3.0.0/etc/hadoop/core-site.xml"))
+                                  command="sudo cp /local/repository/slaves /opt/spark-2.4.0-bin-hadoop2.7/conf/slaves"))
     node.addService(rspec.Execute(shell="/bin/sh",
                                   command="sudo apt-get update -y"))
     node.addService(rspec.Execute(shell="/bin/sh",
-                                  command="sudo apt-get install -y default-jdk"))
+                                  command="sudo apt-get install -y openjdk-8-jdk"))
     node.addService(rspec.Execute(shell="/bin/sh",
                                   command="export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/"))    
     if i != 0:
         node.addService(rspec.Execute(shell="/bin/sh",
                                       command="sudo sleep 30"))
         node.addService(rspec.Execute(shell="/bin/sh",
-                                      command="sudo /opt/spark-2.4.0/bin/hadoop-daemon.sh start datanode"))
+                                      command="sudo /opt/spark-2.4.0-bin-hadoop2.7/sbin/start-master.sh"))
     else:
         node.routable_control_ip = True
-        node.addService(rspec.Execute(shell="/bin/sh",
-                                      command="sudo /opt/hadoop-3.0.0/bin/hdfs namenode -format PEARC18"))
-        node.addService(rspec.Execute(shell="/bin/sh",
-                                      command="sudo /opt/hadoop-3.0.0/bin/hdfs --daemon start namenode"))
+    
+node.addService(rspec.Execute(shell="/bin/sh", command="sudo /opt/spark-2.4.0-bin-hadoop2.7/sbin/start-slaves.sh"))
 
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec(request)
